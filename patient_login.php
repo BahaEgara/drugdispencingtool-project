a6629a9de849b08b91d3f1a59c2a73729981184b
BahaEgara <131773831+BahaEgara@users.noreply.github.com>
2023-06-22 19:52:48 +0300

    Update patient_login.php

--- patient_login.php
+++ patient_login.php
@@ -1,37 +1,30 @@
 <?php
 session_start();
 
+include "connection.php";
+
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the username and password from the form
     $username = $_POST["username"];
     $password = $_POST["pass1"];
 
-    require_once("connection.php");
+    echo $username . " " . $password;
 
-    // Prepare the SQL statement to check the username and password
+    $query = "SELECT * FROM patient WHERE username = '$username' AND pass1 = '$password' LIMIT 1";
 
-    $sql = "SELECT * FROM Patient WHERE username = ? AND pass1 = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $pass1);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    echo "<br>";
 
-    // Check if a row is returned (indicating a successful login)
-    if ($result->num_rows > 0) {
-        // Set the username in the session
+    echo $query;
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
         $_SESSION['username'] = $username;
-        // Redirect the user to the homepage
-        header('Location: patient_homepage.php');
-        exit();
-    } else {
-        // Display an error message for unsuccessful login
-        echo "Login unsuccessful. Incorrect username or password.";
+        header("Location: patient_homepage.php");
     }
 
-    // Close the database connection
-    $stmt->close();
-    $conn->close();
+
 }
 ?>
 
@@ -55,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" placeholder="Enter username" id="username" name="username" required><br><br>
 
         <label for="pass1">Password:</label>
-        <input type="password" placeholder="Enter password" id="pass1" name="pass1" required><br><br>
+        <input type="text" placeholder="Enter password" id="pass1" name="pass1" required><br><br>
 
         <button type="submit">Login</button>
         <a href="welcome_page.php"><button type="button" class="cancel">Cancel</button></a><br>
