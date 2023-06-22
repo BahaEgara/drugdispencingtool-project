86213254669bcca9bb91bfc5ea383b033e550a77
BahaEgara <131773831+BahaEgara@users.noreply.github.com>
2023-06-22 19:54:33 +0300

    Update doctor_login.php

--- doctor_login.php
+++ doctor_login.php
@@ -1,42 +1,38 @@
 <?php
 session_start();
 
+include "connection.php";
+
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the username and password from the form
     $username = $_POST["username"];
     $password = $_POST["pass1"];
 
-    require_once("connection.php");
+    // require_once("connection.php");
 
-    // Prepare the SQL statement to check the username and password
-    
-    $sql = "SELECT * FROM Doctor WHERE username = ? AND pass1 = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $pass1);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    echo $username . " " . $password;
 
-    // Check if a row is returned (indicating a successful login)
-    if ($result->num_rows > 0) {
-        // Set the username in the session
+    $query = "SELECT * FROM doctor WHERE username = '$username' AND pass1 = '$password' LIMIT 1";
+
+    echo "<br>";
+
+    echo $query;
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
         $_SESSION['username'] = $username;
-        // Redirect the user to the homepage
-        header('Location: doctor_homepage.php');
-        exit();
-    } else {
-        // Display an error message for unsuccessful login
-        echo "Login unsuccessful. Incorrect username or password.";
+        header("Location: doctor_homepage.php");
     }
 
-    // Close the database connection
-    $stmt->close();
-    $conn->close();
+
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Doctor Login Page</title>
     <style>
@@ -45,11 +41,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     </style>
 </head>
+
 <body>
     <form method="POST">
 
-    <h2>Doctor login</h2>
-    
+        <h2>Doctor login</h2>
+
         <label for="username">Username:</label>
         <input type="text" placeholder="Enter username" id="username" name="username" required><br><br>
 
@@ -62,4 +59,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <a style="font-family: cursive;" href="doctor_registration_form.php">Don't have an account? Sign up</a>
     </form>
 </body>
+
 </html>
